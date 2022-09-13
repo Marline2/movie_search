9e35ea3400d6a521d7c1608508ccff812443ca77
unknown <marlinelee2@gmail.com>
2022-09-13 15:40:30 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "~/components/HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      message: "hello",
+    };
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,6 @@
+<template>
+  <img 
+    src="~assets/leaf.jpg" 
+    alt="leaf" />
+  <div></div>
+</template>
